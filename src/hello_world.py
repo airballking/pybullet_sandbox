44072846b91d69c64ede1596a2fa80f02754d91f
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2018-01-08 16:01:27 +0100

    Hello World tutorial from docu.

---
+++ src/hello_world.py
@@ -0,0 +1,20 @@
+import pybullet as p
+import pybullet_data
+import rospy
+
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #used by loadURDF
+p.setGravity(0,0,-10)
+planeId = p.loadURDF("plane.urdf")
+cubeStartPos = [0,0,1]
+cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
+boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
+
+#p.stepSimulation()
+p.setRealTimeSimulation(1)
+
+cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
+print(cubePos,cubeOrn)
+rospy.sleep(5)
+print('Time is up')
+p.disconnect()
