95c9f0afad53e3627dfd505a573c35c7aa337a3c
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2018-01-10 10:21:09 +0100

    Gravity compensation for mug.

--- src/pouring_world.py
+++ src/pouring_world.py
@@ -1,18 +1,36 @@
 import pybullet as p
 import pybullet_data
+import numpy as np
 import rospy
 
-physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #used by loadURDF
-p.setGravity(0,0,-9.81)
-planeId = p.loadURDF("plane.urdf")
-cubeStartPos = [0,0,1]
-cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-mug_id = p.loadURDF("/home/gbartels/ros/giskard/src/pybullet_sandbox/data/mug/mug.urdf")
-# mug_id = p.loadSDF("data/mug/mug.sdf", cubeStartPos, cubeStartOrientation)
+class PouringWorld(object):
+    def __init__(self):
+        self.physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+        self.period = 0.01
+        self.gravity_vector = np.array([0,0,-9.81])
+        p.setAdditionalSearchPath(pybullet_data.getDataPath()) #used by loadURDF
+        p.setGravity(self.gravity_vector[0], self.gravity_vector[1], self.gravity_vector[2])
+        p.setRealTimeSimulation(0)
+        planeId = p.loadURDF("plane.urdf")
+        self.mug_id = p.loadURDF("/home/gbartels/ros/giskard/src/pybullet_sandbox/data/mug/mug.urdf", [0,0,1])
 
-p.setRealTimeSimulation(1)
+    def __del__(self):
+        print('Cleaning up')
+        p.disconnect()
 
-rospy.sleep(5)
-print('Time is up')
-p.disconnect()
+    def run(self):
+        p.setTimeStep(self.period, self.physicsClient)
+        rate = rospy.Rate(1.0/self.period)
+        while not rospy.is_shutdown():
+            self.compensate_gravity(self.mug_id)
+            p.stepSimulation(self.physicsClient)
+            rate.sleep()
+
+    def compensate_gravity(self, object_id, link_id = -1):
+        mass = p.getDynamicsInfo(object_id, link_id)[0]
+        p.applyExternalForce(object_id, link_id, (-mass * self.gravity_vector).tolist(), [0, 0, 0], p.LINK_FRAME)
+
+if __name__ == '__main__':
+    rospy.init_node("joint_state_bag_comdr")
+    my_world = PouringWorld()
+    my_world.run()
